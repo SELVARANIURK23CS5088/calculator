ef4386a304703fa8cea0d22a4a5a4c42993089f9
SELVARANIURK23CS5088 <joyselva1310@gmail.com>
2024-07-27 08:58:39 +0530

    Create calculator.js

---
+++ calculator.js
@@ -0,0 +1,10 @@
+
+<script>
+    const buttons = document.querySelectorAll("#btn");
+      const display = document.getElementById("display");
+      buttons.forEach((button) => {
+        button.addEventListener("click", () => {
+          display.value += button.value;
+        });
+      });
+</script>
